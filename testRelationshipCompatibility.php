e0e3d28f4c04f6b9377c2fb9464a3fa2d46c9fda
Larry Reaves <larry@yrral.net>
2012-04-23 20:40:03 -0400

    convert <br /> to br()

--- testRelationshipCompatibility.php
+++ testRelationshipCompatibility.php
@@ -84,6 +84,11 @@ if ($birth_data[0]['sex'] == 'male' && $birth_data[1]['sex'] == 'female') {
   echo "Error: can not find male and female data";
 }
 
+function br()
+{
+	return "<br />";
+}
+
 $male_report = new AstroReport($male_data); 
 
 $female_report = new AstroReport($female_data); 
@@ -92,127 +97,99 @@ $calculator = new AnalyzeKutas( $male_report, $female_report );
 $calculator->prepareKutaReport();
 
 echo "Male Nakshatra: ";
-echo $calculator->male_nakshatra;
-echo "<br />";
+echo $calculator->male_nakshatra, br();
 
 echo "Female Nakshatra: ";
-echo $calculator->female_nakshatra;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_nakshatra, br(), br();
 
 echo "Nadi Kuta Score: ";
 echo $calculator->nadiKutaScore;
-echo "/8<br />";
+echo "/8", br();
 
 echo "Male Dosha: ";
-echo $calculator->male_dosha;
-echo "<br />";
+echo $calculator->male_dosha, br();
 
 echo "Female Dosha: ";
-echo $calculator->female_dosha;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_dosha, br(), br();
 
 echo "Rashi Kuta Score: ";
 echo $calculator->rashiKutaScore;
-echo "/7<br />";
+echo "/7", br();
 
 echo "Male Moon Sign Lord: ";
-echo $calculator->male_moon_sign_lord;
-echo "<br />";
+echo $calculator->male_moon_sign_lord, br();
 
 echo "Female Moon Sign Lord: ";
-echo $calculator->female_moon_sign_lord;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_moon_sign_lord, br(), br();
 
 echo "Gana Kuta Score: ";
 echo $calculator->ganaKutaScore;
-echo "/6<br />";
+echo "/6", br();
 
 echo "Male Gana: ";
-echo $calculator->male_gana;
-echo "<br />";
+echo $calculator->male_gana, br();
 
 echo "Female Gana: ";
-echo $calculator->female_gana;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_gana, br(), br();
 
 echo "Graha Maitri Score: ";
 echo $calculator->grahaMaitriScore;
-echo "/5<br />";
+echo "/5", br();
 
 echo "Male Moon Lord's Relationship with Female Moon Lord: ";
-echo $calculator->m2f_moon_sign_lord_relationship;
-echo "<br />";
+echo $calculator->m2f_moon_sign_lord_relationship, br();
 
 echo "Female Moon Lord's Relationship with Male Moon Lord: ";
-echo $calculator->f2m_moon_sign_lord_relationship;
-echo "<br />";
-echo "<br />";
+echo $calculator->f2m_moon_sign_lord_relationship, br(), br();
 
 echo "Yoni Kuta Score: ";
 echo $calculator->yoniKutaScore;
-echo "/4<br />";
+echo "/4", br();
 echo "Male Yoni: ";
 echo $calculator->male_yoni_sex;
 echo " ";
-echo $calculator->male_yoni;
-echo "<br />";
+echo $calculator->male_yoni, br();
 
 echo "Female Yoni: ";
 echo $calculator->female_yoni_sex;
 echo " ";
-echo $calculator->female_yoni;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_yoni, br(), br();
 
 echo "Dina Kuta Score: ";
 echo $calculator->dinaKutaScore;
-echo "/3<br />";
+echo "/3", br();
 echo "Remainder: ";
-echo $calculator->dinaRemainder;
-echo "<br />";
-echo "<br />";
+echo $calculator->dinaRemainder, br(), br();
 
 echo "Vasya Kuta Score: ";
 echo $calculator->vasyaKutaScore;
-echo "/2<br />";
+echo "/2", br();
 
 echo "Male Rashi: ";
-echo $calculator->male_rashi;
-echo "<br />";
+echo $calculator->male_rashi, br();
 
 echo "Female Rashi: ";
-echo $calculator->female_rashi;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_rashi, br(), br();
 
 echo "Varna Kuta Score: ";
 echo $calculator->varnaKutaScore;
-echo "/1<br />";
+echo "/1", br();
 
 echo "Male Varna: ";
-echo $calculator->male_varna;
-echo "<br />";
+echo $calculator->male_varna, br();
 
 echo "Female Varna: ";
-echo $calculator->female_varna;
-echo "<br />";
-echo "<br />";
+echo $calculator->female_varna, br(), br();
 
 echo "Total Score: ";
 echo $calculator->totalKutaScore;
-echo "/36<br /><br />";
+echo "/36", br(), br();
 
 echo "Kuja Dosha: ";
-echo $calculator->kujaDosha;
-echo "<br />";
+echo $calculator->kujaDosha, br();
 
 echo "Male Kuja Dosha: ";
-echo $calculator->male_kuja_dosha;
-echo "<br />";
+echo $calculator->male_kuja_dosha, br();
 
 echo "Female Kuja Dosha: ";
 echo $calculator->female_kuja_dosha;
